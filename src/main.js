98f9d56f15a904d463a9d8d82bb4e8ea77bf0c74
zach.dyer <zach.dyer@gmail.com>
2020-05-17 12:21:46 -0400

    proof routes are working

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
-
+import Splash from './components/Splash.vue'
+import Register from './components/Register.vue'
+import Signin from './components/Signin.vue'
+import Pricing from './components/Pricing.vue'
+import Dashboard from './components/Dashboard.vue'
+import Checkout from './components/Checkout.vue'
 Vue.config.productionTip = false
-
+Vue.use(VueRouter)
+var router = new VueRouter({
+  routes: [
+    { path: '/', component: Splash },
+    { path: '/register', component: Register },
+    { path: '/signin', component: Signin },
+    { path: '/pricing', component: Pricing },
+    { path: '/checkout', component: Checkout },
+    { path: '/dashboard', component: Dashboard }
+  ]
+})
 new Vue({
+  router: router,
   render: h => h(App),
 }).$mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: [
+      '.codeanyapp.com'
+    ]
+  },
+  outputDir: "../vue-cli-build",
+  publicPath: "/vue-cli-build/"
+};
